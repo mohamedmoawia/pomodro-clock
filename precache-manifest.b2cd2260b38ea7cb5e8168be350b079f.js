494c70dc0b34bae8ef70e1c1268154255611c076
mohamedmoawia <mo3wia94@gmail.com>
2020-06-01 15:47:16 +0200

    Updates

---
+++ precache-manifest.b2cd2260b38ea7cb5e8168be350b079f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c311b03cf21729cf909e687eb2349fbb",
+    "url": "/pomodro-clock/index.html"
+  },
+  {
+    "revision": "f6d8757ef53c8e679aec",
+    "url": "/pomodro-clock/static/css/main.2c390feb.chunk.css"
+  },
+  {
+    "revision": "6f2db771d517fa2b2489",
+    "url": "/pomodro-clock/static/js/2.3ef256a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodro-clock/static/js/2.3ef256a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d8757ef53c8e679aec",
+    "url": "/pomodro-clock/static/js/main.2a96183d.chunk.js"
+  },
+  {
+    "revision": "2cce81f48b6e9ec6f6a7",
+    "url": "/pomodro-clock/static/js/runtime-main.e13aea28.js"
+  }
+]);
